8d2370be697bc6624b0fa95280081eeca40eb157
Mateus Bessa <mateusbessacontato@gmail.com>
2023-12-07 13:15:54 -0300

    Navbar and Footer, JSX and CSS

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,8 @@ import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom"
 //Pages
 import Home from './pages/Home/Home'
 import About from './pages/About/About'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -15,12 +17,14 @@ function App() {
   return (
     <>
       <BrowserRouter>
+        <Navbar />
         <div className="container">
           <Routes>
             <Route path='/' element={<Home />} />
             <Route path='/about' element={<About />} />
           </Routes>
         </div>
+        <Footer />
       </BrowserRouter>
     </>
   )
