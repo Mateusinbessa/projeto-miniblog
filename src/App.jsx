6e40ec91262332b3eeeb63c3a73a9d89b9360441
Mateus Bessa <mateusbessacontato@gmail.com>
2023-12-07 13:47:33 -0300

    JSX do formulário

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,8 @@ import Home from './pages/Home/Home'
 import About from './pages/About/About'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
+import Login from './pages/Login/Login'
+import Register from './pages/Register/Register'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -22,6 +24,8 @@ function App() {
           <Routes>
             <Route path='/' element={<Home />} />
             <Route path='/about' element={<About />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/register' element={<Register />} />
           </Routes>
         </div>
         <Footer />
